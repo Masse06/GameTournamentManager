35f766633a9ccec2dee5294796ba4955c5ae7d8e
danimt <martinezdani4646@gmail.com>
2025-02-19 13:04:13 +0100

    Add the basic structure of the program

---
+++ Player.java
@@ -0,0 +1,80 @@
+public class Player {
+    private String nameTag;
+    private String name;
+    private String surname;
+    private int level;
+    private int points;
+
+    public Player(String nameTag, String name, String surname, int level, int points) {
+        this.nameTag = nameTag;
+        this.name = name;
+        this.surname = surname;
+        this.level = level;
+        this.points = points;
+    }
+
+    public Player() {
+    }
+
+    public String getNameTag() {
+        return nameTag;
+    }
+
+    public void setNameTag(String nameTag) {
+        this.nameTag = nameTag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getPuntos() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Player:" +
+                "\nnameTag='" + nameTag +
+                "\nname='" + name +
+                "\nsurname='" + surname +
+                "\nlevel=" + level +
+                "\npoints=" + points;
+    }
+    
+    // Methods
+    
+    public void winPoints() {
+        this.points += 10;
+    }
+
+    public void upgradeLevel() {
+        if (this.points >= 100 && this.level < 100) {
+            this.level++;
+        }
+    }
+}
---
+++ Tournament.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Tournament {
+    public ArrayList<Player> players = new ArrayList<>();
+
+    public void addPlayer() {
+
+    }
+
+    public void facePlayers() {
+
+    }
+
+    public void showRanking() {
+
+    }
+}
